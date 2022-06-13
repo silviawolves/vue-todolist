9a5a28a112fb9f30f6047215326d7dd024138492
silviawolves <silviacanepa.w@gmail.com>
2022-06-13 13:21:50 +0200

    codice base + consegna

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+new Vue({
+    el: '#app',
+})
+
+
+
+
+/*
+Descrizione:
+Rifare l’esercizio della to do list.
+Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+MILESTONE 1
+Stampare all’interno di una lista, un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item una “x”: cliccando su di essa, senza chiedere all’utente conferma, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale (sempre visibile) e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+*/
